8189430e442733975dc1dd427ab0cf6878c4cc6f
luandro <luandro@gmail.com>
2021-11-03 13:00:04 -0300

    remove prisma

--- src/events.ts
+++ src/events.ts
@@ -1,155 +1,11 @@
 import { TextDecoder } from "util";
 
 import { Protobuf } from "@meshtastic/meshtasticjs";
-import type { Hardware as HardwareType } from "@prisma/client";
-import pkg from "@prisma/client";
-import { connection, prisma } from "./index.js";
+import { connection } from "./index.js";
 import { postMessage } from "./ssb";
 
-const { Hardware } = pkg;
-
-const addNode = async (node: Protobuf.NodeInfo): Promise<void> => {
-  await prisma.node.upsert({
-    where: {
-      number: node.num,
-    },
-    create: {
-      number: node.num,
-      lastHeard: new Date(node.lastHeard * 1000),
-    },
-    update: {
-      lastHeard: new Date(node.lastHeard * 1000),
-    },
-  });
-
-  if (node.user) {
-    addUser(node.user, node.num);
-  }
-
-  if (node.position) {
-    addPosition(node.position, node.num);
-  }
-
-  if (node.num !== 0xffffffff) {
-    await prisma.sNR.create({
-      data: {
-        snr: node.snr,
-        node: {
-          connect: {
-            number: node.num,
-          },
-        },
-      },
-    });
-  }
-};
-
-const addUser = async (user: Protobuf.User, nodeNum: number): Promise<void> => {
-  await prisma.user.upsert({
-    where: {
-      userId: user.id,
-    },
-    create: {
-      userId: user.id,
-      longName: user.longName,
-      shortName: user.shortName,
-      hardware: user.hwModel
-        ? Hardware[Protobuf.HardwareModel[user.hwModel ?? 0] as HardwareType]
-        : Hardware.ANDROID_SIM,
-      node: {
-        connect: {
-          number: nodeNum,
-        },
-      },
-      mac:
-        Buffer.from(user.macaddr)
-          .toString("hex")
-          .match(/.{1,2}/g)
-          ?.join(":") ?? "",
-    },
-    update: {
-      longName: user.longName,
-      shortName: user.shortName,
-    },
-  });
-};
-
-const addPosition = async (
-  position: Protobuf.Position,
-  nodeNum: number
-): Promise<void> => {
-  if (
-    position.latitudeI === 0 &&
-    position.longitudeI === 0 &&
-    position.altitude === 0 &&
-    position.batteryLevel === 0
-  ) {
-    return;
-  }
-
-  const newPosition = await prisma.position.create({
-    data: {
-      latitude: position.latitudeI / 1e7,
-      longitude: position.longitudeI / 1e7,
-      altitude: position.altitude,
-      batteryLevel: position.batteryLevel,
-      node: {
-        connect: {
-          number: nodeNum,
-        },
-      },
-    },
-  });
-  await prisma.node.update({
-    where: {
-      number: nodeNum,
-    },
-    data: {
-      position: {
-        connect: {
-          id: newPosition.id,
-        },
-      },
-    },
-  });
-};
 
 export const RegisterSubscribers = () => {
-  connection.onMeshPacket.subscribe(async (packet) => {
-    if (packet.to === 0xffffffff) {
-      await addNode(
-        Protobuf.NodeInfo.create({
-          num: 0xffffffff,
-        })
-      );
-    }
-    const nodes = await prisma.node.findMany({
-      where: {
-        number: {
-          in: [packet.from, packet.to],
-        },
-      },
-    });
-
-    await Promise.all(
-      [packet.from, packet.to].map(async (number) => {
-        nodes.find((node) => node.number === number) ||
-          (await addNode(
-            Protobuf.NodeInfo.create({
-              num: number,
-            })
-          ));
-      })
-    );
-  });
-  connection.onNodeInfoPacket.subscribe(async (packet) => {
-    await addNode(packet.data);
-  });
-
-  connection.onPositionPacket.subscribe(async (packet) => {
-    await addPosition(packet.data, packet.packet.from);
-  });
-
   connection.onTextPacket.subscribe(async (packet) => {
     const nodeNum = connection?.myNodeInfo?.myNodeNum;
     if (nodeNum !== packet.packet.from) {
@@ -158,9 +14,9 @@ export const RegisterSubscribers = () => {
           type: "post",
           text: packet.data,
           loraTo:
-            packet.packet.to === 0xffffffff ? -1 : packet.packet.to.toString(),
-          loraPacketId: packet.packet.id.toString(),
-          loraFrom: packet.packet.from.toString(),
+            packet.packet.to === 0xffffffff ? -1 : packet.packet.to.toFixed(2),
+          loraPacketId: packet.packet.id.toFixed(2),
+          loraFrom: packet.packet.from.toFixed(2),
           loraChannel: packet.packet.channel,
           loraRxSnr: packet.packet.rxSnr,
         };
@@ -181,6 +37,5 @@ export const RegisterSubscribers = () => {
   });
 
   connection.onUserPacket.subscribe(async (packet) => {
-    await addUser(packet.data, packet.packet.from);
   });
 };
--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,14 @@ import {
   Protobuf,
   SettingsManager,
 } from "@meshtastic/meshtasticjs";
-import pkg from "@prisma/client";
 import { App } from "@tinyhttp/app";
-import { resolve } from "path/posix";
-import { json } from "milliparsec";
-import { getPosts, postMessage } from "./ssb";
 import { RegisterSubscribers } from "./events.js";
 
-const { PrismaClient } = pkg;
 
 const app = new App();
 
 SettingsManager.debugMode = Protobuf.LogRecord_Level.TRACE;
 export const connection = new IHTTPConnection();
-export const prisma = new PrismaClient();
-// const deviceIP = "10.208.237.230";
 const deviceIP = "meshtastic.local";
 
 RegisterSubscribers();
@@ -33,81 +26,3 @@ async function connect() {
 }
 
 connect()
-
-app
-  .get("/", async (_, res) => {
-    // const posts = await getPosts();
-    // console.log("POSTS", posts);
-    res.status(200);
-  })
-  .get("/connect", async (_, res) => {
-    await connect();
-    res.json(connection.url);
-  })
-
-  //API Routes,
-  .get("/messages", async (_, res) => {
-    res.json(await prisma.message.findMany());
-  })
-  .post("/message", async (req, res) => {
-    await json()(req, res, (err) => void err && console.log(err));
-    const stringified = JSON.stringify(req.body);
-    // const message = JSON.stringify(req.body);
-    console.log("🚀 ~ file: index.ts ~ line 44 ~ .post ~ message", req.body);
-    // if (!message || !message.content) res.json({ status: 500 });
-    await connection.sendText(stringified, undefined, true);
-    res.json({ status: 200 });
-  })
-  .get("/users", async (_, res) => {
-    res.json(await prisma.user.findMany());
-  })
-  .get("/nodes", async (_, res) => {
-    res.json(
-      await prisma.node.findMany({
-        include: {
-          user: true,
-        },
-      })
-    );
-  })
-  .get("/positions", async (_, res) => {
-    res.json(await prisma.position.findMany());
-  })
-  .get("/user/:id", async (req, res) => {
-    res.json(
-      await prisma.user.findUnique({
-        where: {
-          id: req.params.id,
-        },
-      })
-    );
-  })
-  .get("/node/:id", async (req, res) => {
-    res.json(
-      await prisma.node.findUnique({
-        where: {
-          id: req.params.id,
-        },
-      })
-    );
-  })
-  .get("/node/:id/position", async (req, res) => {
-    res.json(
-      await prisma.node.findUnique({
-        where: {
-          id: req.params.id,
-        },
-      })
-    );
-  })
-  .get("/node/:id/positions", async (req, res) => {
-    res.json(
-      await prisma.node.findUnique({
-        where: {
-          id: req.params.id,
-        },
-      })
-    );
-  })
-
-  .listen(3000, () => console.log("Started on http://localhost:3000"));
