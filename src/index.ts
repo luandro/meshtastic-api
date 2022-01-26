25eabae395f665a86368fd870b9530760e4cda23
luandro <luandro@gmail.com>
2022-01-26 15:17:28 -0300

    testing with ISerialConnection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {
+  ISerialConnection,
   IHTTPConnection,
   Protobuf,
   SettingsManager,
@@ -11,17 +12,28 @@ const app = new App();
 
 SettingsManager.debugMode = Protobuf.LogRecord_Level.TRACE;
 export const connection = new IHTTPConnection();
+export const serialConnection = new ISerialConnection();
 const deviceIP = "meshtastic.local";
 
 RegisterSubscribers();
 async function connect() {
-  console.log("Connecting to", deviceIP);
-  await connection.connect({
+  // const ports = await serialConnection.getPorts()
+  // console.log("🚀 ~ file: index.ts ~ line 22 ~ connect ~ ports", ports)
+  // TODO: Check if serial or wifi
+  const httpParams = {
     address: deviceIP,
     tls: false,
     receiveBatchRequests: false,
     fetchInterval: 2000,
-  });
+  }
+  // const serialParams = {
+  //   baudRate: 18282,
+  //   port: ports[0]
+  // }
+  // TODO: run wifi connection if no serial
+  console.log("Connecting to", deviceIP);
+  await connection.connect(httpParams);
+  // await serialConnection.connect(serialParams);
   console.log("Connected");
 }
 
